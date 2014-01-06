ebce46b74371e16b817c251bb076b22387c3b6f3
= <=>
2014-01-06 23:44:11 +0000

    removed unneccesary closing p tag from posts_helper truncate method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,6 @@ module PostsHelper
     if truncate_index = post.body_html.index(Post::TRUNCATE_MARKER)
       post.body_html = truncate(post.body_html, :length => truncate_index + 1, :omission => " ", :escape => false)
       post.body_html += link_to('Read more...', post_path(post))
-      post.body_html += '</p>'.html_safe
     end
     post
   end
