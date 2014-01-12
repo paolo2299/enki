fc68ab6bcd04a6510ed596f4044f25b60f5301b0
= <=>
2014-01-12 23:20:56 +0000

    HOMEPAGE - WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @welcome_message = "Welcome to pdlawson.com"
+    @profile_image_src = "/images/paul-profile-pic.jpg"
+    @description = <<-desc
+    I'm a software developer living in London, with a passion for technology.
+    On this website you'll find my blog, etc. etc. software projects etc etc.
+    desc
   end
 end
